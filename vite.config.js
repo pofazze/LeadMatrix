f3bfbe294c3165336ed7f76c0da5519a878478d5
pofazze <pofazze@gmail.com>
2025-07-18 11:59:43 -0300

    Remoção do proxy

--- vite.config.js
+++ vite.config.js
@@ -7,19 +7,6 @@ export default defineConfig({
   server: {
     host: true, // Permite acesso via LAN
     port: 5173, // (Opcional) Garante a porta 5173
-    proxy: {
-      '/api': {
-        target: 'https://primary-lhz6-production.up.railway.app',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '/api'),
-      },
-      '/webhook': {
-        target: 'https://primary-lhz6-production.up.railway.app',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/webhook/, '/webhook'),
-      },
-    },
+
   },
 })
